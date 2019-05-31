3c6174ac96664bc45e84e0b88d7cc91ff757f3f3
Max David <me@maxdavid.com>
2019-05-31 12:37:12 -0700

    Update app to refresh weather every 15 minutes

--- src/pkjs/app.js
+++ src/pkjs/app.js
@@ -192,11 +192,12 @@ Pebble.addEventListener("ready", function(e) {
   setInterval(function() {
     //console.log("timer fired");
     updateWeather();
-  }, 60000); // 1 minutes
-//  }, 300000); // 5 minutes
-//  }, 600000); // 10 minutes
-//  }, 1200000); // 20 minutes
-//  }, 1800000); // 30 minutes
+  }, 900000); // 15 minutes
+  // }, 60000); // 1 minute
+  // }, 300000); // 5 minutes
+  // }, 600000); // 10 minutes
+  // }, 1200000); // 20 minutes
+  // }, 1800000); // 30 minutes
 
   console.log(e.type);
 });
